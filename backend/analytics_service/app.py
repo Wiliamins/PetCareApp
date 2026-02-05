bdbe1ad313f2d10ef52a990ae9968360097f333a
VS <wiliyamin101001@gmail.com>
2026-02-05 03:15:49 +0100

    fix: allow proxy host for Render

--- backend/analytics_service/app.py
+++ backend/analytics_service/app.py
@@ -13,6 +13,10 @@ import time
 app = Flask(__name__)
 CORS(app)
 
+from werkzeug.middleware.proxy_fix import ProxyFix
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1, x_port=1)
+
 SERVICES = {
 
     'auth': {
